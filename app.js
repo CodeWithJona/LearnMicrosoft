1722eb02f7d9d3c9cf4349f594bc9896af338e3a
Jonathan Olbes <olbesdev@outlook.com>
2023-05-05 06:50:28 -0500

    Nueva rama

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
-let myString1 = "Hello"
-let myString2 = "World"
+const STARTING_POKER_CHIPS = 100; // points
+const PLAYERS = 3;
+const NO_OF_STARTER_CARDS = 2
+let gameHasEnded = false
 
-let concatString = myString1 + myString2 + "!"
-let concatString2 = myString1 + ' ' + myString2 + "!"
+let playerOneName = "Chloe"
+let playerTwoName = "Jasmine"
+let playerThreeName = "Jen"
 
-console.log(concatString)
+console.log(`Welcome to Texas Hold'em. The championship title will be awared to one of these three players:
+
+${playerOneName}, ${playerTwoName} and ${playerThreeName}. Each player has ${STARTING_POKER_CHIPS } in their pot.
+
+We have an excisting game ahead of us. May the best player win!`)
+
+let playerOnePoints = STARTING_POKER_CHIPS
+let playerTwoPoints = STARTING_POKER_CHIPS
+let playerThreePoints = STARTING_POKER_CHIPS
+
+playerOnePoints = 0
+playerTwoPoints = 0
+playerThreePoints = 75
+
+gameHasEnded =  ((playerOnePoints + playerTwoPoints) == 0) ||
+                ((playerTwoPoints + playerThreePoints) == 0) ||
+                ((playerOnePoints + playerThreePoints) == 0)
+                
+console.log("Game has ended", gameHasEnded)
+                
